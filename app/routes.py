e9157f5376b265feab76f4518f681787321988a7
Thomas Marcello <thomasmarcello@gmail.com>
2024-04-17 16:29:08 -0700

    Created empty files

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from flask import Flask         # from the flask module import the Flask class
+
+app = Flask(__name__)           # Create an instance of the Flask class. It is the OOP (Object oriented paradigm)
+
+@app.get("/")                   # Flask decorator that maps view functions to routes
+def index():                    # view function
+   me = {                       # python dictionary
+      "first_name": "Thomas",
+      "last_name": "Marcello",
+      "hobbies": "Cooking",
+      "is_online": True
+   }
+   return me                    # When you return a dict from a view function, it becomes JSON                   
